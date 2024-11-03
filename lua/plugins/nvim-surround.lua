a3d1d10d00fc93e8d25dda5b0b42980bc05990fa
ianTevesAcc <iantevesbusinessacc@outlook.com>
2024-11-03 22:54:44 +1300

    backup

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -2,11 +2,11 @@ return {
   "kylechui/nvim-surround",
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
-  lazy = 'VeryLazy',
-
   config = function()
     require("nvim-surround").setup({
       -- Configuration here, or leave empty to use defaults
+      vim.api.nvim_set_keymap("n", "+", "ysiw", { noremap = true, silent = true }),
+      vim.keymap.set("v", "+", "S", { noremap = true, silent = true }),
     })
-  end,
+  end
 }
